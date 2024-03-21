0d089cd6bc371cbd697aa3a7bb371a6458ccaea1
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-03-21 18:17:37 +0000

    A calculator web-app implement using reactjs.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Layout/>
+        <Layout />
       </div>
-    );
+    )
   };
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,57 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "./Layout.css";
+import logo from "../../assets/img/logo.png";
+import Output from '../Output/Output';
 
-class Layout extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       message: `hey`
+const Layout = props => {
+  let [input, setInput] = useState("0");
+  let [result, setResult] = useState("");
+
+  const handleClick = (event) => {
+    const value = event.target.value;
+    if(value === "="){
+      if(input != ""){
+        let res = ""
+        try{
+          res = eval(input)
+        } catch(err){
+          setResult("Match error")
+        }
+        if (res === undefined){
+          setResult("Math error")
+        } else {
+          setResult(input + "=")
+          setInput(res)
+        }
+      }
+    } else if(value === "C"){
+      setInput("0")
+      setResult("")
+    } else if (value === "DEL"){
+      let str = input
+      str = str.substring(0, str.length - 1)
+      setInput(str)
+    } else if (input === "0"){
+      setInput(value)
+    } else{
+      setInput((input += value))
     }
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = () => {
-    return `hey`;
   };
+  
+  return (
+    <div className='frame'>
+      <div className='calculator'>
+        <br></br>
+        <Output user={input} answer={result} />
+        <img src={logo} width="60px" style={{display: "block", marginLeft: "auto", marginRight: "auto"}} />
+        <div className='keys'>
+          <input type='button' value={'C'} className='button clear' onClick={handleClick}></input>
+           <input type='button' value={'DEL'} className='button clear' onClick={handleClick}></input>
+           <input type='button' value={'%'} className='button operator' onClick={handleClick}></input>
+          <input type='button' value={'/'} className='button operator' onClick={handleClick}></input>
 
-  render() {
-    return (
-      <div className='frame'>
-        <div className='calculator'>
-          <br></br>
-          <img />
-          <div className='keys'>
-            <input type='button' value={'C'} className='button clear' onClick={this.handleClick}></input>
-            <input type='button' value={'DEL'} className='button clear' onClick={this.handleClick}></input>
-            <input type='button' value={'%'} className='button operator' onClick={this.handleClick}></input>
-            <input type='button' value={'/'} className='button operator' onClick={this.handleClick}></input>
+          <input type='button' value={'7'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'8'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'9'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'*'} className='button operator' onClick={handleClick}></input>
 
-            <input type='button' value={'7'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'8'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'9'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'*'} className='button operator' onClick={this.handleClick}></input>
+          <input type='button' value={'4'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'5'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'6'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'-'} className='button operator' onClick={handleClick}></input>
+          <input type='button' value={'1'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'2'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'3'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'+'} className='button operator' onClick={handleClick}></input>
 
-            <input type='button' value={'4'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'5'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'6'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'-'} className='button operator' onClick={this.handleClick}></input>
-
-            <input type='button' value={'1'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'2'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'3'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'+'} className='button operator' onClick={this.handleClick}></input>
-
-            <input type='button' value={'0'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'.'} className='button' onClick={this.handleClick}></input>
-            <input type='button' value={'='} className='button equal-sign' onClick={this.handleClick}></input>
-          </div>
-
+          <input type='button' value={'0'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'.'} className='button' onClick={handleClick}></input>
+          <input type='button' value={'='} className='button equal-sign' onClick={handleClick}></input>
         </div>
       </div>
-    );
-  };
+    </div>
+  )
 }
 
 export default Layout;
